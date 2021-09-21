fbf6900d6d2c822192e7129eb32a7cd05cf11488
MarvinKlein <marvin.klein@audi.de>
2021-09-21 11:22:56 +0200

    Small Bug fixes

--- src/main/java/Placebs.java
+++ src/main/java/Placebs.java
@@ -4,31 +4,31 @@ public class Placebs {
     public String[][] placebs(String[][] grid, String inputdriection, int pos1, int pos2) {
         Scanner sc = new Scanner(System.in);
         if (inputdriection.equals("v")) {
-            while (pos1 >2 || grid[pos1 + 1][pos2].equals("X") || grid[pos1][pos2].equals("X") || grid[pos1 + 2][pos2].equals("X")) {
+            while (pos1 >2 || !grid[pos1 + 1][pos2].equals("0") || !grid[pos1][pos2].equals("0") || !grid[pos1 + 2][pos2].equals("0")) {
                 //System.out.println("\nPlease enter anoter line where no ship is and the grid is not expanded: ");
                 pos1 = randomnumber();
                 //System.out.println("Please enter anoter collum where no ship is and the grid is not expanded: ");
                 pos2 = randomnumber();
             }
-            grid[pos1 + 2][pos2] = "X";
-            grid[pos1 + 1][pos2] = "X";
-            grid[pos1][pos2] = "X";
+            grid[pos1 + 2][pos2] = "B";
+            grid[pos1 + 1][pos2] = "B";
+            grid[pos1][pos2] = "B";
 
         } else if (inputdriection.equals("h")) {
-            while (pos2 >2 || grid[pos1][pos2].equals("X") || grid[pos1][pos2 + 1].equals("X") || grid[pos1][pos2 + 2].equals("X")) {
+            while (pos2 >2 || !grid[pos1][pos2].equals("0") || !grid[pos1][pos2 + 1].equals("0") || !grid[pos1][pos2 + 2].equals("0")) {
                 //System.out.println("\nPlease enter anoter line where no ship is and the grid is not expanded: ");
                 pos1 = randomnumber();
                 //System.out.println("Please enter anoter collum where no ship is and the grid is not expanded: ");
                 pos2 = randomnumber();
             }
-            grid[pos1][pos2] = "X";
-            grid[pos1][pos2 + 1] = "X";
-            grid[pos1][pos2 + 2] = "X";
+            grid[pos1][pos2] = "B";
+            grid[pos1][pos2 + 1] = "B";
+            grid[pos1][pos2 + 2] = "B";
         }
         return grid;
     }
     public static int randomnumber() {
-        int random = (int) (Math.random() * 4);
+        int random = (int) (Math.random() * 5);
         return random;
     }
 }
